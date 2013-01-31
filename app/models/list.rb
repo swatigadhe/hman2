e5119bd5c2fcb5c910697d918544b22f6249dde4
swatigadhe <gadhe.swati@gmail.com>
2013-01-31 17:49:41 +0530

    chnaes

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,7 +3,8 @@ class List < ActiveRecord::Base
   attr_accessible :words, :hint
   validates :words, :presence => true
   validates :words, :uniqueness => true
-  
+  validates :hint, :presence => true
+  validates :hint, :uniqueness => true
   def self.random_word
     List.find(rand(List.count) + 1).words.upcase
   end
