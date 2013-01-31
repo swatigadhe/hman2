2d2cddf9be2e00f5a073fc16ab1daa65e342d498
swatigadhe <gadhe.swati@gmail.com>
2013-01-31 16:00:46 +0530

    changed

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ActiveRecord::Base
   attr_accessible :gamename
-  has_many :lists
+  has_many :lists, :dependent => :destroy
   validates :gamename, :presence => true
   has_many :usergames
   has_many :users, :through => :usergames
